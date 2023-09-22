f74f14525c073d734ee9e1ab963889a4bf2cf31d
vidhya11062004 <145103024+vidhya11062004@users.noreply.github.com>
2023-09-22 11:50:45 +0530

    Add files via upload

---
+++ challenge unit3/main (16).py
@@ -0,0 +1,25 @@
+"""
+Write a function called linear_search_product that takes the list of products and a target product
+name as input. The function should perform a lenear search to find the target product in the list and
+return a list of indices of all occurrences of the product if found, or an empty list if the product is not
+found.
+"""
+
+
+def lenearSearchProduct(productList, targetproduct):
+  indices = []
+
+  for index, product in enumerate(productList):
+   if product == targetproduct:
+     indices.append(index)
+
+  return indices
+
+
+# Example usage:
+product = ["shoes", "boot", "loafer", "shoes", "sandles", "shoes"]
+target = "shoes"
+result = lenearSearchProduct(product, target)
+print(result)
+  
+  
